8eb2c0dec91103fc54364df9da2ff29baaa41984
automationwithnaveen <automationwithnaveen@gmail.com>
2025-07-26 09:33:42 +0100

    no more requi

--- src/com/flexiquiz/pageobjects/GITTestExmple.java
+++
@@ -1,9 +0,0 @@
-package com.flexiquiz.pageobjects;
-
-public class GITTestExmple
-{
-	String idno;
-	String name;
-	String age;
-	
-}
--- src/com/flexiquiz/pageobjects/SampleGIT.java
+++
@@ -1,6 +0,0 @@
-package com.flexiquiz.pageobjects;
-
-public class SampleGIT 
-{
-	// This is only for sample test examples
-}
